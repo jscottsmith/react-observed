db2a2089c6375dce04f5f1dede79706d3775872c
J Smith <jscsmith@gmail.com>
2017-10-21 22:51:25 -0700

    add story example

--- demo/components/Element/Element.stories.js
+++ demo/components/Element/Element.stories.js
@@ -7,10 +7,23 @@ import { linkTo } from '@storybook/addon-links';
 import { Element } from 'components';
 import Observed from 'react-observed';
 
-storiesOf('Observed Element', module).add('with default props', () => (
-    <Observed>
-        {({ isInView, mapRef }) => (
-            <Element isInView={isInView} mapRef={mapRef} />
-        )}
-    </Observed>
-));
+storiesOf('Observed Element', module)
+    .add('with default props', () => (
+        <Observed>
+            {({ isInView, mapRef }) => (
+                <Element isInView={isInView} mapRef={mapRef} />
+            )}
+        </Observed>
+    ))
+    .add('`isInView` when intersecting >= 0.01', () => (
+        <Observed
+            intersectionRatio={0.01}
+            options={{
+                threshold: [0.01, 0.5, 1],
+            }}
+        >
+            {({ isInView, mapRef }) => (
+                <Element isInView={isInView} mapRef={mapRef} />
+            )}
+        </Observed>
+    ));
